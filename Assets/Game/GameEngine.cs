a22fbd1c67f75bae194084d559516a857ea3de4b
Nadun De Silva <nadunrds@gmail.com>
2015-10-17 18:56:17 +0530

    Made the Game Engine a Singleton class

--- Assets/Game/GameEngine.cs
+++ Assets/Game/GameEngine.cs
@@ -14,5 +14,23 @@ namespace Assets.Game
         private List<Water> water;
         private Tank[] tanks;
         private List<Bullet> bullets;
+
+        private static GameEngine instance;
+        public static GameEngine Instance
+        {
+            get
+            {
+                if (instance == null) instance = new GameEngine();
+                return instance;
+            }
+        }
+        private GameEngine()
+        {
+            brickWalls = new List<BrickWall>();
+            stoneWalls = new List<StoneWall>();
+            water = new List<Water>();
+            tanks = new Tank[5];
+            bullets = new List<Bullet>();
+        }
     }
 }
