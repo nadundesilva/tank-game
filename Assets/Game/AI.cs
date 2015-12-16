edaa37492350878ac894056bf8d75ee5b065f112
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2015-12-17 00:44:11 +0530

    AI initiation

--- Assets/Game/AI.cs
+++ Assets/Game/AI.cs
@@ -8,6 +8,9 @@ namespace Assets.Game
          * The depth of the tree created for the min max algorithm
         */
         private int treeDepth;
+        private GameEngine gameEngine ;
+        private GameObject[,] map ;
+        private Tank ownedTank ;
 
         #region Variables for storing references to important objects
         #endregion
@@ -15,16 +18,47 @@ namespace Assets.Game
         public AI()
         {
             treeDepth = Constants.Instance.AITreeDepth;
+            gameEngine = GameManager.Instance.GameEngine;
+            map = gameEngine.Map;
+            ownedTank = gameEngine.Tanks[gameEngine.PlayerNumber];
         }
 
         /*
          * Calculates the best move based on the state of the game
         */
+
+            
+
+        //all possible moves :
+            /*
+             *up
+             *down
+             *left 
+             *right
+             *shoot
+             *one move goes to turn to another direction
+             */
+        /*
+         Objectives
+         * survive from bullet
+         * avoid drawning in water
+         * avoid hitting with a stone wall
+         * colect a health pile
+         * collect coins
+         * shoot others
+         * destroy brick wall
+         * no movement
+         * 
+                 
+         */
+        function 
+
         public void CalculateMove()
         {
-            GameEngine gameEngine = GameManager.Instance.GameEngine;
-            GameObject[,] map = gameEngine.Map;
-            Tank ownedTank = gameEngine.Tanks[gameEngine.PlayerNumber];
+            
+            
+
+
 
         }
     }
